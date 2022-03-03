4275293355c006c70ed35d09d4279499d5529a46
Игорь Егошин <igor.egoshim@gmail.com>
2022-03-04 00:39:27 +0300

    Вторая версия приложения

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -1,8 +1,8 @@
 ﻿using BlogApp.Models;
+using BlogApp.Repositories;
+
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 
 namespace BlogApp
 {
@@ -12,6 +12,8 @@ namespace BlogApp
 
         static void Main( string[] args )
         {
+            IAuthorRepository authorRepository = new AuthorRawSqlRepository( _connectionString );
+
             Console.WriteLine( "Доступные команды:" );
             Console.WriteLine( "get-authors - показать список авторов блога" );
             Console.WriteLine( "add-author - добавить автора" );
@@ -25,7 +27,7 @@ namespace BlogApp
 
                 if ( command == "get-authors" )
                 {
-                    List<Author> authors = GetAuthors();
+                    List<Author> authors = authorRepository.GetAll();
                     foreach ( Author author in authors )
                     {
                         Console.WriteLine( $"Id: {author.Id}, Name: {author.Name}" );
@@ -36,7 +38,7 @@ namespace BlogApp
                     Console.WriteLine( "Введите имя автора" );
                     string name = Console.ReadLine();
 
-                    AddAuthor( new Author
+                    authorRepository.Add( new Author
                     {
                         Name = name
                     } );
@@ -46,7 +48,7 @@ namespace BlogApp
                 {
                     Console.WriteLine( "Введите id автора" );
                     int authorId = int.Parse( Console.ReadLine() );
-                    Author author = GetAuthorById( authorId );
+                    Author author = authorRepository.GetById( authorId );
 
                     if ( author == null )
                     {
@@ -57,21 +59,21 @@ namespace BlogApp
                     Console.WriteLine( "Введите новое имя автора" );
                     author.Name = Console.ReadLine();
 
-                    UpdateAuthor( author );
+                    authorRepository.Update( author );
                     Console.WriteLine( "Успешно обновлено" );
                 }
                 else if ( command == "delete-author" )
                 {
                     Console.WriteLine( "Введите id автора" );
                     int authorId = int.Parse( Console.ReadLine() );
-                    var author = GetAuthorById( authorId );
+                    var author = authorRepository.GetById( authorId );
                     if ( author == null )
                     {
                         Console.WriteLine( "Автор не найден" );
                         continue;
                     }
 
-                    DeleteAuthorById( author.Id );
+                    authorRepository.DeleteById( author.Id );
                     Console.WriteLine( "Успешно удалено" );
                 }
                 else if ( command == "exit" )
@@ -81,125 +83,6 @@ namespace BlogApp
                 else
                 {
                     Console.WriteLine( "Команда не найдена" );
-                }
-            }
-        }
-
-        public static List<Author> GetAuthors()
-        {
-            var result = new List<Author>();
-
-            using ( var connection = new SqlConnection( _connectionString ) )
-            {
-                connection.Open();
-                using ( SqlCommand command = connection.CreateCommand() )
-                {
-                    command.CommandText = "select [Id], [Name] from [Author]";
-
-                    using ( var reader = command.ExecuteReader() )
-                    {
-                        while ( reader.Read() )
-                        {
-                            result.Add( new Author
-                            {
-                                Id = Convert.ToInt32( reader["Id"] ),
-                                Name = Convert.ToString( reader["Name"] )
-                            } );
-                        }
-                    }
-                }
-            }
-
-            return result;
-        }
-
-        public static void AddAuthor( Author author )
-        {
-            using ( var connection = new SqlConnection( _connectionString ) )
-            {
-                connection.Open();
-                using ( SqlCommand command = connection.CreateCommand() )
-                {
-                    command.CommandText =
-                        @"insert into [Author]
-                            ([Name])
-                        values
-                            (@name)
-                        select SCOPE_IDENTITY()";
-
-                    command.Parameters.Add( "@name", SqlDbType.NVarChar ).Value = author.Name;
-
-                    author.Id = Convert.ToInt32( command.ExecuteScalar() );
-                }
-            }
-        }
-
-        public static Author GetAuthorById( int id )
-        {
-            using ( var connection = new SqlConnection( _connectionString ) )
-            {
-                connection.Open();
-                using ( SqlCommand command = connection.CreateCommand() )
-                {
-                    command.CommandText =
-                        @"select [Id], [Name]
-                        from [Author]
-                        where [Id] = @id";
-
-                    command.Parameters.Add( "@id", SqlDbType.Int ).Value = id;
-                    using ( var reader = command.ExecuteReader() )
-                    {
-                        if ( reader.Read() )
-                        {
-                            return new Author
-                            {
-                                Id = Convert.ToInt32( reader["Id"] ),
-                                Name = Convert.ToString( reader["Name"] )
-                            };
-                        }
-                        else
-                        {
-                            return null;
-                        }
-                    }
-                }
-            }
-        }
-
-        public static void UpdateAuthor( Author author )
-        {
-            using ( var connection = new SqlConnection( _connectionString ) )
-            {
-                connection.Open();
-                using ( SqlCommand command = connection.CreateCommand() )
-                {
-                    command.CommandText =
-                        @"update [Author]
-                        set [Name] = @name
-                        where [Id] = @id";
-
-                    command.Parameters.Add( "@name", SqlDbType.NVarChar ).Value = author.Name;
-                    command.Parameters.Add( "@id", SqlDbType.Int ).Value = author.Id;
-
-                    command.ExecuteNonQuery();
-                }
-            }
-        }
-
-        public static void DeleteAuthorById( int id )
-        {
-            using ( var connection = new SqlConnection( _connectionString ) )
-            {
-                connection.Open();
-                using ( SqlCommand command = connection.CreateCommand() )
-                {
-                    command.CommandText =
-                        @"delete [Author]
-                        where [Id] = @id";
-
-                    command.Parameters.Add( "@id", SqlDbType.Int ).Value = id;
-
-                    command.ExecuteNonQuery();
                 }
             }
         }
