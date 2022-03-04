102ff7844e8b4aa93b2f15ff7b4cd96cc2257ce5
Игорь Егошин <igor.egoshim@gmail.com>
2022-03-04 21:27:36 +0300

    Обновил примеры sql и дз

--- Example.sql
+++ Example.sql
@@ -1,40 +1,42 @@
-use Test
+use Blogs
 
 create table Author(
 	Id int identity(1,1) constraint PK_Author primary key,
 	Name nvarchar(100)
 )
 
+select * from Author
+
 insert into Author
 	(Name)
 values
-	('Паша'),
+	('Иван'),
 	('Маша'),
-	('Олег')
-	
-	
-select [Id], [Name] from [Author]
+	('Паша')
 
 update Author
 set Name = 'Олег'
 where Id = 2
 
 delete from Author
-where Id = 3
+where Id = 2
+
 
 create table Post(
 	Id int identity(1,1) constraint PK_Post primary key,
 	Title nvarchar(100),
 	Body nvarchar(max),
-	AuthorId int constraint FK_Author_Post references Author(Id)
+	AuthorId int constraint FK_Post_Author references Author(Id)
 )
+
+select * from Post
+select * from Author
 
 insert into Post
 values
-('Title',
- 'Body',
- 2 )
+('Заголовок 1', 'Тело новости', 3),
+('Заголовок 2', 'Тело новости 2', 3),
+('Заголовок 3', 'Тело новости 3', 1)
 
-select p.Title, p.Body, a.Name AuthorName from Post p
-join Author a on p.AuthorId = a.Id
-
+select p.Title, p.Body, a.Name as AuthorName from Post as p
+join Author a on a.Id = p.AuthorId
